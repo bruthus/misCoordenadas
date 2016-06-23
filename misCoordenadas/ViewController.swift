b335aab3e34230baf0a167888ddc6d4698efe6bf
Sergio Vega <sergio@openhex.com.ar>
2016-06-23 19:21:54 -0300

    Initial Commit

---
+++ misCoordenadas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  misCoordenadas
+//
+//  Created by Sergio Vega on 23/6/16.
+//  Copyright © 2016 OpenHex S.A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
